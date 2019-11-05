61bd912444cf7ef82fca5b82a5c31103f91514a9
cice <cice@192.168.3.117>
2019-11-05 21:50:02 +0100

    cice

--- Ejercicios2Arrays/src/ejercicio/array/Ejerccicio7.java
+++ Ejercicios2Arrays/src/ejercicio/array/Ejerccicio7.java
@@ -1,9 +1,27 @@
 package ejercicio.array;
 
+import java.util.Scanner;
+
 public class Ejerccicio7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner entrada= new Scanner(System.in);
+		int[][]matriz = new int[3][3];
+		for (int fila=0; fila<matriz.length; fila++){
+			for (int col=0; col<matriz[fila].length; col++){
+				System.out.println("Numero:");
+				matriz[fila][col]= entrada.nextInt();
+			}
+		}
+		for (int fila=0; fila<matriz.length; fila++){
+			for (int col=0; col<matriz[fila].length; col++){
+				System.out.println("Numero:");
+				matriz[fila][col]= entrada.nextInt();
+			}
+		}
+		
+		
 
 	}
 
--- Ejercicios2Arrays/src/ejercicio/array/Ejercicio8.java
+++ Ejercicios2Arrays/src/ejercicio/array/Ejercicio8.java
@@ -1,10 +1,28 @@
 package ejercicio.array;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// 6 7
+		// 8 2 3
+		// 9 8 5 4 4
+		Scanner entrada= new Scanner(System.in);
+		
+		
+		int [][] matriz= new int[3][];
+		matriz[0] = new int [2];
+		matriz[1] = new int [3];
+		matriz[2] = new int [5];
+		for (int fila=0; fila<matriz.length; fila++){
+			for (int col=0; col<matriz[fila].length; col++){
+				System.out.println("Numero:");
+				matriz[fila][col]= entrada.nextInt();
+			}
+		}
 	}
 
 }
